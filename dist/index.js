3ac2cbad6aff6b4b33e4d41211484498c8c8d799
gatwirival <valegjohn@gmail.com>
2022-07-30 13:54:18 +0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayGoodbye = exports.sayHello = void 0;
+var Hello_world_1 = require("./Hello-world");
+Object.defineProperty(exports, "sayHello", { enumerable: true, get: function () { return Hello_world_1.sayHello; } });
+Object.defineProperty(exports, "sayGoodbye", { enumerable: true, get: function () { return Hello_world_1.sayGoodbye; } });
---
+++ readme.md
@@ -0,0 +1,9 @@
+Consume  library anywhere you want by running: 
+```
+npm install --save hellowld
+```
+Then use
+```
+import {sayHello} from 'hellowld'
+sayHello();
+```
